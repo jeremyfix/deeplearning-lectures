0422df3d57723199ee6324cd4712ca8f1a94feda
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2019-03-06 16:49:58 +0100

    first iteration with data and models

--- LabsSolutions/00-pytorch-CIFAR100/train.py
+++ LabsSolutions/00-pytorch-CIFAR100/train.py
@@ -1,6 +1,7 @@
-import torchvision.datasets
-
 import argparse
+
+import data
+import models
 
 if __name__ == '__main__':
 
@@ -8,26 +9,56 @@ if __name__ == '__main__':
 
 
     parser.add_argument(
-            '--use_gpu',
-            action='store_true',
-            help='Whether to use GPU'
+        '--use_gpu',
+        action='store_true',
+        help='Whether to use GPU'
     )
 
     parser.add_argument(
         '--dataset_dir',
         type=str,
         help='Where to store the downloaded dataset',
-        default=None
+        required=True
     )
 
     parser.add_argument(
-            '--num_workers',
-            type=int,
-            default=1,
-            help='The number of CPU threads used'
+        '--num_workers',
+        type=int,
+        default=1,
+        help='The number of CPU threads used'
     )
 
+    parser.add_argument(
+        '--model',
+        choices=['linear', 'cnn'],
+        action='store',
+        required=True
+    )
 
     args = parser.parse_args()
 
-    dataset = torchvision.datasets.CIFAR10(args.dataset_dir, train=True, transform=None, target_transform=None, download=False)
+    valid_ratio = 0.2
+    batch_size  = 128
+    num_workers = args.num_workers
+    dataset_dir = args.dataset_dir
+    train_augment_transform = []
+
+    input_dim = (3, 32, 32)
+    num_classes = 100
+
+    # Data loading
+    train_loader, valid_loader = data.load_data(valid_ratio,
+                                                batch_size,
+                                                num_workers,
+                                                dataset_dir,
+                                                train_augment_transform)
+    # Model definition
+    model = models.build_model(args.model, input_dim, num_classes)
+
+    # Loss function
+
+    # Callbacks
+
+    # Training loop
+
+
