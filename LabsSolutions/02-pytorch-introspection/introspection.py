9341f0d2d717286ebbfc5613fd3e08b2375c7603
Jerem <jeremy.fix@centralesupelec.fr>
2019-11-29 15:54:39 +0100

    adding a resnet50 and processing of an image. The output logits do not seem to be the right with testing with banana image, I got argmax at 600 while the banana is class id 955

--- LabsSolutions/02-pytorch-introspection/introspection.py
+++ LabsSolutions/02-pytorch-introspection/introspection.py
@@ -7,6 +7,9 @@ demangling what is learned by a deep neural network
 For saliency_simonyan
     Simonyan et al. (2014), Deep Inside Convolutional Networks: VisualisingImage Classification Models and Saliency Maps. https://arxiv.org/pdf/1312.6034.pdf
 
+Additional references:
+    Montavon et al. (2017) Methods for Interpreting and Understanding Deep Neural Networks, https://arxiv.org/pdf/1706.07979.pdf
+
 """
 
 
@@ -15,7 +18,31 @@ import argparse
 # External modules
 import torch
 import torchvision
-import torchvision.transforms as transforms
+from PIL import Image
+# Local modules
+import models
+
+
+def saliency_simonyan(device, args):
+    """
+    Takes a pretrained model and an input image and computes the 
+    saliency over this input image according to [Simonyan et al.(2014)]
+    """
+    # Checks we have the required arguments
+    if not args.image:
+        raise RuntimeError("I need an input image to work")
+
+    # Loads a pretrained model
+    image_transform, model = models.get_model("resnet50", device)
+
+    # Loads the image
+    img = Image.open(args.image).convert('RGB')
+
+    # Go through the model
+    input_tensor = image_transform(img).to(device).unsqueeze(0)
+    out = model(input_tensor)
+    print(torch.argmax(out), out.shape)
+
 
 if __name__ == '__main__':
 
@@ -28,14 +55,22 @@ if __name__ == '__main__':
                         action='store',
                         required=True)
 
+    parser.add_argument('--image',
+                       type=str,
+                       action='store',
+                       help='The input image to process if required')
+
     args = parser.parse_args()
 
     use_gpu = torch.cuda.is_available()
     if use_gpu:
+        print("Using the GPU")
         device = torch.device('cuda')
     else:
+        print("Using the CPU")
         device = torch.device('cpu')
 
-    # Load a model pretrained on ImageNet
-    model = torchvision.models.resnet50(pretrained=True)
-    model.to(device)
+    exec("{}(device, args)".format(args.visu))
+
+
+
