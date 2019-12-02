05ab2acee45abb4fd80fe5fe015fee5fd0c524c4
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2019-12-02 15:36:56 +0100

    fix: should use device for add_graph inputs

--- LabsSolutions/00-pytorch-FashionMNIST/train.py
+++ LabsSolutions/00-pytorch-FashionMNIST/train.py
@@ -172,7 +172,9 @@ Optimizer
                                              {'model': model, 'normalization_function': normalization_function}
                                             )
     # Add the graph of the model to the tensorboard
-    tensorboard_writer.add_graph(model, next(iter(train_loader))[0])
+    inputs, _ = next(iter(train_loader))[0]
+    inputs = inputs.to(device)
+    tensorboard_writer.add_graph(model, inputs)
     ####################################################################################### Main Loop
     for t in range(epochs):
         print("Epoch {}".format(t))
