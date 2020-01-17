7916985ad47816204cca766167034274adf23528
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2020-01-17 16:14:27 +0100

    creating a train function which takes a config dictionnary compatible with tune.

--- LabsSolutions/00-pytorch-FashionMNIST/train.py
+++ LabsSolutions/00-pytorch-FashionMNIST/train.py
@@ -18,66 +18,15 @@ import utils
 import models
 import data
 
-if __name__ == '__main__':
+def train(config):
+    '''
+    Run a training with the provided parameters
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        '--use_gpu',
-        action='store_true',
-        help='Whether to use GPU'
-    )
-
-    parser.add_argument(
-        '--dataset_dir',
-        type=str,
-        help='Where to store the downloaded dataset',
-        default=None
-    )
-
-    parser.add_argument(
-        '--num_workers',
-        type=int,
-        default=1,
-        help='The number of CPU threads used'
-    )
-
-    parser.add_argument(
-        '--weight_decay',
-        type=float,
-        default=0,
-        help='Weight decay'
-    )
-
-    parser.add_argument(
-        '--data_augment',
-        help='Specify if you want to use data augmentation',
-        action='store_true'
-    )
-
-    parser.add_argument(
-        '--normalize',
-        help='Which normalization to apply to the input data',
-        action='store_true'
-    )
-
-    parser.add_argument(
-        '--logdir',
-        type=str,
-        default="./logs",
-        help='The directory in which to store the logs'
-    )
-
-    parser.add_argument(
-        '--model',
-        choices=['linear', 'fc', 'fcreg', 'vanilla', 'fancyCNN'],
-        action='store',
-        required=True
-    )
-
-
-    args = parser.parse_args()
-
+    config (dict):
+        use_gpu : True/False
+        log : None or dict
+        model : linear, fc, fcreg, vanilla, fancyCNN
+    '''
     img_width = 28
     img_height = 28
     img_size = (1, img_height, img_width)
@@ -85,8 +34,9 @@ if __name__ == '__main__':
     batch_size=128
     epochs=10
     valid_ratio=0.2
+    tensorlogs = config['log'] is not None
 
-    if args.use_gpu:
+    if config['use_gpu']:
         print("Using GPU{}".format(torch.cuda.current_device()))
         device = torch.device('cuda')
     else:
@@ -95,12 +45,14 @@ if __name__ == '__main__':
 
 
     # Where to store the logs
-    logdir = utils.generate_unique_logpath(args.logdir, args.model)
-    print("Logging to {}".format(logdir))
-    if not os.path.exists(args.logdir):
-        os.mkdir(args.logdir)
-    if not os.path.exists(logdir):
-        os.mkdir(logdir)
+    if tensorlogs: 
+        logdir = utils.generate_unique_logpath(config['log']['logdir'], 
+                                               config['model'])
+        print("Logging to {}".format(logdir))
+        if not os.path.exists(config['log']['logdir']):
+            os.mkdir(config['log']['logdir'])
+        if not os.path.exists(logdir):
+            os.mkdir(logdir)
 
     # FashionMNIST dataset
     train_augment_transforms = None
@@ -111,9 +63,10 @@ if __name__ == '__main__':
 
     train_loader, valid_loader, test_loader, normalization_function = data.load_fashion_mnist(valid_ratio,
                                                                                               batch_size,
-                                                                                              args.num_workers,
-                                                                                              args.normalize,
-                                                                                              dataset_dir = args.dataset_dir,
+                                                                                              config['num_workers'],
+                                                                                              config['normalize'],
+                                                                                              dataset_dir =
+                                                                                              config['dataset_dir'],
                                                                                               train_augment_transforms = train_augment_transforms)
 
 
@@ -199,20 +152,90 @@ Optimizer
         tensorboard_writer.add_scalar('metrics/test_acc',  test_acc, t)
 
 
-# Loading the best model found
+    # Loading the best model found
 
-print("Loading and testing the best model")
+    print("Loading and testing the best model")
 
-best_model_path = logdir + "/best_model.pt"
-loaded_dict = torch.load(best_model_path)
-model = loaded_dict['model'].to(device)
+    best_model_path = os.path.join(config['logdir'], "best_model.pt")
+    loaded_dict = torch.load(best_model_path)
+    model = loaded_dict['model'].to(device)
 
-model.eval()
+    model.eval()
 
-val_loss, val_acc = utils.test(model, valid_loader, loss, device)
-print(" Validation : Loss : {:.4f}, Acc : {:.4f}".format(val_loss, val_acc))
+    val_loss, val_acc = utils.test(model, valid_loader, loss, device)
+    print(" Validation : Loss : {:.4f}, Acc : {:.4f}".format(val_loss, val_acc))
 
-test_loss, test_acc = utils.test(model, test_loader, loss, device)
-print(" Test       : Loss : {:.4f}, Acc : {:.4f}".format(test_loss, test_acc))
+    test_loss, test_acc = utils.test(model, test_loader, loss, device)
+    print(" Test       : Loss : {:.4f}, Acc : {:.4f}".format(test_loss, test_acc))
 
 
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--use_gpu',
+        action='store_true',
+        help='Whether to use GPU'
+    )
+
+    parser.add_argument(
+        '--dataset_dir',
+        type=str,
+        help='Where to store the downloaded dataset',
+        default=None
+    )
+
+    parser.add_argument(
+        '--num_workers',
+        type=int,
+        default=1,
+        help='The number of CPU threads used'
+    )
+
+    parser.add_argument(
+        '--weight_decay',
+        type=float,
+        default=0,
+        help='Weight decay'
+    )
+
+    parser.add_argument(
+        '--data_augment',
+        help='Specify if you want to use data augmentation',
+        action='store_true'
+    )
+
+    parser.add_argument(
+        '--normalize',
+        help='Which normalization to apply to the input data',
+        action='store_true'
+    )
+
+    parser.add_argument(
+        '--logdir',
+        type=str,
+        default="./logs",
+        help='The directory in which to store the logs'
+    )
+
+    parser.add_argument(
+        '--model',
+        choices=['linear', 'fc', 'fcreg', 'vanilla', 'fancyCNN'],
+        action='store',
+        required=True
+    )
+
+    args = parser.parse_args()
+
+    config = {
+        'use_gpu': args.use_gpu,
+        'log' : {'logdir': args.logdir},
+        'model': args.model,
+        'num_workers': args.num_workers,
+        'normalize': args.normalize,
+        'dataset_dir': args.dataset_dir
+    }
+
+
+    train(config)
