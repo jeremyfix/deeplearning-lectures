d6485b9bc914b58e0e7679e5cabd3e953c981e68
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2022-01-28 22:21:28 +0100

    lower learning rate

--- LabsSolutions/00-pytorch-FashionMNISTLightning/train.py
+++ LabsSolutions/00-pytorch-FashionMNISTLightning/train.py
@@ -38,7 +38,7 @@ class LitModel(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.model.parameters(),
-                                     lr=0.01,
+                                     lr=0.001,
                                      weight_decay=self.weight_decay)
         scheduler = lr_scheduler.ReduceLROnPlateau(optimizer)
         return {"optimizer": optimizer,
