82d8849505ec3b988e475ae7a9453a9073183112
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2022-03-21 06:45:41 +0100

    this script does not parse the commit id

--- LabsSolutions/02-pytorch-asr/job.py
+++ LabsSolutions/02-pytorch-asr/job.py
@@ -53,7 +53,7 @@ echo ""
 echo "Training"
 date
 
-python3 main_ctc.py  --logname ctc_${{SLURM_ARRAY_JOB_ID}}_${{SLURM_ARRAY_TASK_ID}} --commit_id '{commit_id}' --baselogdir ${{current_dir}}/logs train
+python3 main_ctc.py  --logname ctc_${{SLURM_ARRAY_JOB_ID}}_${{SLURM_ARRAY_TASK_ID}} --baselogdir ${{current_dir}}/logs train
 
 if [[ $? != 0 ]]; then
     exit -1
