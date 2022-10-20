2969694afcedc4568a4e42267103a2492e2ff2d5
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2022-10-20 22:14:43 +0200

    style fix, onnx export

--- LabsSolutions/00-pytorch-FashionMNIST/utils.py
+++ LabsSolutions/00-pytorch-FashionMNIST/utils.py
@@ -4,20 +4,21 @@ from torch.utils.data import Dataset
 from torch.nn.modules.module import _addindent
 from torch.utils.data.sampler import SubsetRandomSampler
 
+
 def train(model, loader, f_loss, optimizer, device):
     """
-        Train a model for one epoch, iterating over the loader
-        using the f_loss to compute the loss and the optimizer
-        to update the parameters of the model.
+    Train a model for one epoch, iterating over the loader
+    using the f_loss to compute the loss and the optimizer
+    to update the parameters of the model.
 
-        Arguments :
-        model     -- A torch.nn.Module object
-        loader    -- A torch.utils.data.DataLoader
-        f_loss -- The loss function, i.e. a loss Module
-        optimizer -- A torch.optim.Optimzer object
-        use_gpu  -- Boolean, whether to use GPU
+    Arguments :
+    model     -- A torch.nn.Module object
+    loader    -- A torch.utils.data.DataLoader
+    f_loss -- The loss function, i.e. a loss Module
+    optimizer -- A torch.optim.Optimzer object
+    use_gpu  -- Boolean, whether to use GPU
 
-        Returns :
+    Returns :
 
     """
 
@@ -55,8 +56,13 @@ def train(model, loader, f_loss, optimizer, device):
         optimizer.step()
 
         # Display status
-        progress_bar(i, len(loader), msg = "Loss : {:.4f}, Acc : {:.4f}".format(tot_loss/N, correct/N))
-    return tot_loss/N, correct/N
+        progress_bar(
+            i,
+            len(loader),
+            msg="Loss : {:.4f}, Acc : {:.4f}".format(tot_loss / N, correct / N),
+        )
+    return tot_loss / N, correct / N
+
 
 def test(model, loader, f_loss, device):
     """
@@ -98,7 +104,7 @@ def test(model, loader, f_loss, device):
             # For the accuracy
             predicted_targets = outputs.argmax(dim=1)
             correct += (predicted_targets == targets).sum().item()
-        return tot_loss/N, correct/N
+        return tot_loss / N, correct / N
 
 
 # from https://github.com/kuangliu/pytorch-cifar/blob/master/utils.py
@@ -108,21 +114,23 @@ import sys
 import time
 import math
 
-_, term_width = os.popen('stty size', 'r').read().split()
+_, term_width = os.popen("stty size", "r").read().split()
 term_width = int(term_width)
 
-TOTAL_BAR_LENGTH = 65.
+TOTAL_BAR_LENGTH = 65.0
 last_time = time.time()
 begin_time = last_time
+
+
 def progress_bar(current, total, msg=None):
     global last_time, begin_time
     if current == 0:
         begin_time = time.time()  # Reset for new bar.
 
-    cur_len = int(TOTAL_BAR_LENGTH*current/total)
+    cur_len = int(TOTAL_BAR_LENGTH * current / total)
     rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
 
-    sys.stdout.write(' [{}>{}]'.format('='*cur_len, '.'*rest_len))
+    sys.stdout.write(" [{}>{}]".format("=" * cur_len, "." * rest_len))
 
     cur_time = time.time()
     step_time = cur_time - last_time
@@ -130,71 +138,72 @@ def progress_bar(current, total, msg=None):
     tot_time = cur_time - begin_time
 
     L = []
-    L.append('  Step: %10s' % format_time(step_time))
-    L.append(' | Tot: %10s' % format_time(tot_time))
+    L.append("  Step: %10s" % format_time(step_time))
+    L.append(" | Tot: %10s" % format_time(tot_time))
     if msg:
-        L.append(' | ' + msg)
+        L.append(" | " + msg)
 
-    msg = ''.join(L)
+    msg = "".join(L)
     sys.stdout.write(msg)
-    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
-        sys.stdout.write(' ')
+    for i in range(term_width - int(TOTAL_BAR_LENGTH) - len(msg) - 3):
+        sys.stdout.write(" ")
 
     # Go back to the center of the bar.
-    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
-        sys.stdout.write('\b')
-    sys.stdout.write(' %d/%d ' % (current+1, total))
+    for i in range(term_width - int(TOTAL_BAR_LENGTH / 2) + 2):
+        sys.stdout.write("\b")
+    sys.stdout.write(" %d/%d " % (current + 1, total))
 
-    if current < total-1:
-        sys.stdout.write('\r')
+    if current < total - 1:
+        sys.stdout.write("\r")
     else:
-        sys.stdout.write('\n')
+        sys.stdout.write("\n")
     sys.stdout.flush()
 
+
 def format_time(seconds):
-    days = int(seconds / 3600/24)
-    seconds = seconds - days*3600*24
+    days = int(seconds / 3600 / 24)
+    seconds = seconds - days * 3600 * 24
     hours = int(seconds / 3600)
-    seconds = seconds - hours*3600
+    seconds = seconds - hours * 3600
     minutes = int(seconds / 60)
-    seconds = seconds - minutes*60
+    seconds = seconds - minutes * 60
     secondsf = int(seconds)
     seconds = seconds - secondsf
-    millis = int(seconds*1000)
+    millis = int(seconds * 1000)
 
-    f = ''
+    f = ""
     i = 1
     if days > 0:
-        f += str(days) + 'D'
+        f += str(days) + "D"
         i += 1
     if hours > 0 and i <= 2:
-        f += str(hours) + 'h'
+        f += str(hours) + "h"
         i += 1
     if minutes > 0 and i <= 2:
-        f += str(minutes) + 'm'
+        f += str(minutes) + "m"
         i += 1
     if secondsf > 0 and i <= 2:
-        f += str(secondsf) + 's'
+        f += str(secondsf) + "s"
         i += 1
     if millis > 0 and i <= 2:
-        f += str(millis) + 'ms'
+        f += str(millis) + "ms"
         i += 1
-    if f == '':
-        f = '0ms'
+    if f == "":
+        f = "0ms"
     return f
 
 
 def generate_unique_logpath(logdir, raw_run_name):
     i = 0
-    while(True):
+    while True:
         run_name = raw_run_name + "_" + str(i)
         log_path = os.path.join(logdir, run_name)
         if not os.path.isdir(log_path):
             return log_path
         i = i + 1
 
-class ModelCheckpoint:
 
+class ModelCheckpoint:
     def __init__(self, filepath, dict_to_save):
         self.min_loss = None
         self.filepath = filepath
@@ -203,20 +212,23 @@ class ModelCheckpoint:
     def update(self, loss):
         if (self.min_loss is None) or (loss < self.min_loss):
             print("Saving a better model")
-            #torch.save(self.model.state_dict(), self.filepath)
+            # torch.save(self.model.state_dict(), self.filepath)
             torch.save(self.dict_to_save, self.filepath)
             self.min_loss = loss
+            return True
+        return False
+
 
 # https://stackoverflow.com/questions/42480111/model-summary-in-pytorch
 def torch_summarize(model, show_weights=True, show_parameters=True):
     """Summarizes torch model by showing trainable parameters and weights."""
-    tmpstr = model.__class__.__name__ + ' (\n'
+    tmpstr = model.__class__.__name__ + " (\n"
     total_params = 0
     for key, module in model._modules.items():
         # if it contains layers let call it recursively to get params and weights
         if type(module) in [
             torch.nn.modules.container.Container,
-            torch.nn.modules.container.Sequential
+            torch.nn.modules.container.Sequential,
         ]:
             modstr = torch_summarize(module)
         else:
@@ -227,16 +239,13 @@ def torch_summarize(model, show_weights=True, show_parameters=True):
         total_params += params
         weights = tuple([tuple(p.size()) for p in module.parameters()])
 
-        tmpstr += '  (' + key + '): ' + modstr
+        tmpstr += "  (" + key + "): " + modstr
         if show_weights:
-            tmpstr += ', weights={}'.format(weights)
+            tmpstr += ", weights={}".format(weights)
         if show_parameters:
-            tmpstr +=  ', parameters={}'.format(params)
-        tmpstr += '\n'
+            tmpstr += ", parameters={}".format(params)
+        tmpstr += "\n"
 
-    tmpstr = tmpstr + ')'
-    tmpstr += '\n {} learnable parameters'.format(total_params)
+    tmpstr = tmpstr + ")"
+    tmpstr += "\n {} learnable parameters".format(total_params)
     return tmpstr
-
-
-
