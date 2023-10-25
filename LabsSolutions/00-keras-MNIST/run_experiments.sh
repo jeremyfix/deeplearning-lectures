a6a0ff710932574fa0107560e38286081a24dbe4
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2023-10-25 19:27:14 +0200

    fix new interface

--- LabsSolutions/00-keras-MNIST/run_experiments.sh
+++ LabsSolutions/00-keras-MNIST/run_experiments.sh
@@ -5,9 +5,9 @@ NB_RUNS=10
 for iter in $(seq 1 $NB_RUNS)
 do
     echo "Run $iter"
-    python3 train.py --model linear
-    python3 train.py --model fc --minimize --dropout --L2
-    python3 train.py --model vanilla --normalize --dropout --L2
-    python3 train.py --model fancy --normalize --dropout --L2
-    python3 train.py --model fancy --normalize --dropout --L2 --data_augment
+    python3 main.py --model linear train
+    python3 main.py --model fc --minimize --dropout --L2 train
+    python3 main.py --model vanilla --normalize --dropout --L2 train
+    python3 main.py --model fancy --normalize --dropout --L2 train
+    python3 main.py --model fancy --normalize --dropout --L2 --data_augment train
 done
