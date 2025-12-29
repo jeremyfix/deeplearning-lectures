733e096b736b9a6694ab29d75ee59d7e757b9bb7
Jeremy Fix <jeremy.fix@gmail.com>
2025-12-29 20:10:24 +0100

    refactor the models submodule

--- LabsSolutions/pytorch/asr/src/asrlab/models/__init__.py
+++ LabsSolutions/pytorch/asr/src/asrlab/models/__init__.py
@@ -1,3 +1,6 @@
 # coding: utf-8
 
 from .deepspeech import CTCModel
+
+def build_model(charmap, cfg):
+    return eval(f"{cfg['class']}(charmap, **cfg['params'])")
--- LabsSolutions/pytorch/asr/src/asrlab/models/__main__.py
+++ LabsSolutions/pytorch/asr/src/asrlab/models/__main__.py
@@ -1,79 +1,102 @@
 # coding: utf-8
 
 # Standard imports
+import sys
+
 # External imports
 import torch
 
 # Local imports
-from .deepspeech import CTCModel
-from asrlab import utils, data
-
-def build_ctc_model():
-    charmap = data.CharMap()
-
-    return CTCModel(
-        charmap, n_mels=80, nhidden_rnn=185, nlayers_rnn=3, cell_type="GRU", dropout=0.1
-    )
-
+from asrlab import data, models, utils
 
 def test_model_cnn():
     utils.head("Testing the cnn part")
+    
+    cfg = {
+        'class': "CTCModel",
+        'params':
+        {
+            'n_mels': 80,
+            'nhidden_rnn': 185,
+            'nlayers_rnn': 3,
+            'cell_type': "GRU",
+            'dropout': 0.1
+        }
+    }
 
-    try:
-        T, B = 124, 10
-        model = build_ctc_model()
+    T, B = 124, 10
+    charmap = data.CharMap()
+    model = models.build_model(charmap, cfg)
 
-        cnn_inputs = torch.randn((T, B, model.n_mels)).transpose(0, 1).unsqueeze(dim=1)
-        out_cnn = model.cnn(cnn_inputs)
+    cnn_inputs = torch.randn((T, B, model.n_mels)).transpose(0, 1).unsqueeze(dim=1)
+    out_cnn = model.cnn(cnn_inputs)
 
-        utils.info(f"Got an output of shape {out_cnn.shape}")
-        expected_shape = [10, 32, 31, 40]
-        if list(out_cnn.shape) == expected_shape:
-            utils.succeed()
-        else:
-            utils.fail(f"was expecting {expected_shape}")
-    except:
-        utils.fail(f"{sys.exc_utils.info()[0]}")
+    utils.info(f"Got an output of shape {out_cnn.shape}")
+    expected_shape = [10, 32, 31, 40]
+    if list(out_cnn.shape) == expected_shape:
+        utils.succeed()
+    else:
+        utils.fail(f"was expecting {expected_shape}")
 
 
 def test_model_rnn():
     utils.head("Testing the rnn part")
 
-    try:
-        T, B = 124, 10
-        model = build_ctc_model()
+    cfg = {
+        'class': "CTCModel",
+        'params':
+        {
+            'n_mels': 80,
+            'nhidden_rnn': 185,
+            'nlayers_rnn': 3,
+            'cell_type': "GRU",
+            'dropout': 0.1
+        }
+    }
 
-        rnn_inputs = torch.randn((T, B, 1280))
-        out_rnn, _ = model.rnn(rnn_inputs)
+    T, B = 124, 10
+    charmap = data.CharMap()
+    model = models.build_model(charmap, cfg)
 
-        utils.info(f"Got an output of shape {out_rnn.shape}")
-        expected_shape = [124, 10, 370]
-        if list(out_rnn.shape) == expected_shape:
-            utils.succeed()
-        else:
-            utils.fail(f"was expecting {expected_shape}")
-    except:
-        utils.fail(f"{sys.exc_utils.info()[0]}")
+    rnn_inputs = torch.randn((T, B, 1280))
+    out_rnn, _ = model.rnn(rnn_inputs)
+
+    utils.info(f"Got an output of shape {out_rnn.shape}")
+    expected_shape = [124, 10, 370]
+    if list(out_rnn.shape) == expected_shape:
+        utils.succeed()
+    else:
+        utils.fail(f"was expecting {expected_shape}")
 
 
 def test_model_out():
     utils.head("Testing the output part")
 
-    try:
-        T, B = 124, 10
-        model = build_ctc_model()
+    cfg = {
+        'class': "CTCModel",
+        'params':
+        {
+            'n_mels': 80,
+            'nhidden_rnn': 185,
+            'nlayers_rnn': 3,
+            'cell_type': "GRU",
+            'dropout': 0.1
+        }
+    }
 
-        out_inputs = torch.randn((T, B, 370))
-        out_out = model.charlin(out_inputs)
+    T, B = 124, 10
+    charmap = data.CharMap()
+    model = models.build_model(charmap, cfg)
 
-        utils.info(f"Got an output of shape {out_out.shape}")
-        expected_shape = [124, 10, 44]
-        if list(out_out.shape) == expected_shape:
-            utils.succeed()
-        else:
-            utils.fail(f"was expecting {expected_shape}")
-    except:
-        utils.fail(f"{sys.exc_utils.info()[0]}")
+    out_inputs = torch.randn((T, B, 370))
+    out_out = model.charlin(out_inputs)
+
+    utils.info(f"Got an output of shape {out_out.shape}")
+    expected_shape = [124, 10, 44]
+    if list(out_out.shape) == expected_shape:
+        utils.succeed()
+    else:
+        utils.fail(f"was expecting {expected_shape}")
 
 
 if __name__ == "__main__":
