8e15cc5f5dbbde5c51d2e88c66f10b825b054f91
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2025-12-30 20:09:09 +0100

    reduce the execution time to 2h

--- LabsSolutions/pytorch/asr/submit-slurm.py
+++ LabsSolutions/pytorch/asr/submit-slurm.py
@@ -12,7 +12,7 @@ def makejob(commit_id, configpath):
 #SBATCH --job-name=asrlab
 #SBATCH --nodes=1
 #SBATCH --partition=gpu_prod_long
-#SBATCH --time=48:00:00
+#SBATCH --time=2:00:00
 #SBATCH --output=logslurms/slurm-%A_%a.out
 #SBATCH --error=logslurms/slurm-%A_%a.err
 
