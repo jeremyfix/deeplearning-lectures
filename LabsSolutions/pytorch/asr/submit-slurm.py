566316622335e20f1515dd58b8acfaa69b19b5e5
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2025-12-30 20:46:56 +0100

    reintroduce the copy of the data

--- LabsSolutions/pytorch/asr/submit-slurm.py
+++ LabsSolutions/pytorch/asr/submit-slurm.py
@@ -25,9 +25,9 @@ echo "Running on " $(hostname)
 
 echo "Copying the data"
 date
-# mkdir -p $TMPDIR/CommonVoice
-# cp -r /mounts/datasets/datasets/CommonVoice/cv-corpus-24.0-2025-12-05 $TMPDIR/CommonVoice/
-# ln -s $TMPDIR/CommonVoice/cv-corpus-24.0-2025-12-05 $TMPDIR/CommonVoice/v24.0 
+mkdir -p $TMPDIR/CommonVoice
+cp -r /mounts/datasets/datasets/CommonVoice/cv-corpus-24.0-2025-12-05 $TMPDIR/CommonVoice/
+ln -s $TMPDIR/CommonVoice/cv-corpus-24.0-2025-12-05 $TMPDIR/CommonVoice/v24.0 
 date
 
 echo "Copying the config file"
