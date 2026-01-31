ff290de7a535144e6082487f7846ecfd765e1e2d
Jeremy Fix <jeremy.fix@gmail.com>
2026-01-31 09:28:30 +0100

    sampler: fix transpose, fix usage of batch size, adds tqdm

--- LabsSolutions/pytorch/nir/src/nirlab/samplers/image.py
+++ LabsSolutions/pytorch/nir/src/nirlab/samplers/image.py
@@ -9,6 +9,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 from PIL import Image
+import tqdm
 
 # Local imports
 import nirlab.utils as utils
@@ -41,17 +42,17 @@ def generate_sample(model,
         points = utils.build_coordinate_Nd(width, height, device=device) # torch tensor (width*height, dim_input)
         
         # Build a dataloader from it
-        batch_size = 32
+        batch_size = batch_size
         dataloader = torch.utils.data.DataLoader(points, batch_size=batch_size, shuffle=False, drop_last=False)
 
         # Loop over batches
         idx = 0
-        for batch_points in dataloader:
+        for batch_points in tqdm.tqdm(dataloader):
             out_batch = model(batch_points)  # (B, dim_output)
             img[idx:idx+batch_points.shape[0], :] = out_batch.detach().cpu().numpy()
             idx += batch_points.shape[0]
 
-        img = img.reshape((height, width, dim_output))
+        img = img.reshape((width, height, dim_output)).transpose((1, 0, 2))  # (height, width, dim_output)
 
         # Convert to [0, 255] range
         img = img * 255.0
@@ -78,7 +79,7 @@ def sample_image(model,
     if save_arr.ndim == 3 and save_arr.shape[2] == 1:
         save_arr = save_arr[:, :, 0]
     Image.fromarray(save_arr).save(filename)
-    logging.info(f"Saved sample image to {filename}.png")
+    logging.info(f"Saved sample image to {filename}")
 
 
 def test_sampler():
